80bffc5f538b2517864ea1cdbfdc132d84e37e37
moskal-alicja <moskal.ala@gmail.com>
2019-12-01 18:15:01 +0100

    Initial project for backend

---
+++ backend/parkly-backend/src/main/java/pw/react/backend/parklybackend/ParklyBackendApplication.java
@@ -0,0 +1,13 @@
+package pw.react.backend.parklybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParklyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParklyBackendApplication.class, args);
+	}
+
+}
